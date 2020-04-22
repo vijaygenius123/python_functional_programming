134ac248d884dd19c60af24047a521532b05dd90
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-22 19:10:30 +0530

    Assign Function To Variable Based On Condition

--- 01.py
+++ 01.py
@@ -4,3 +4,20 @@ def say_hello(name):
 say_hello2 = say_hello
 
 say_hello2('Vijay')
+
+ENVIRONMENT = 'dev'
+
+def fetch_data_real():
+    print('Calling API')
+
+def fetch_data_fake():
+    print('Returning Fake Data')
+    return {
+        'name': 'Vijay',
+        'age': 26
+    }
+
+fetch_data = fetch_data_real if ENVIRONMENT == 'prod' else fetch_data_fake
+
+data = fetch_data()
+print(data)
